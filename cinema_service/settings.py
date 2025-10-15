2b3005422568e25ef155303319350bda4f644ae6
Platon Goncharevskyi <platongoncharevskyi@gmail.com>
2025-10-16 00:00:17 +0300

    upd2

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,10 +137,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "files" / "static"
+STATIC_ROOT = "/files/static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "files" / "media"
+MEDIA_ROOT = "/files/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
